7667c90b18c0740b664d6d0bf7ad716a88f656f0
Pungitius <robert.kosmehl@gmx.de>
2023-11-15 18:05:05 +0100

    get scene vertices

--- src/DemoScene.ts
+++ src/DemoScene.ts
@@ -1,35 +1,56 @@
-import * as THREE from 'three'
+import * as THREE from "three";
 
-import { OBJLoader } from 'three/examples/jsm/Addons.js';
-import { MTLLoader } from 'three/examples/jsm/Addons.js';
+import { OBJLoader } from "three/examples/jsm/Addons.js";
+import { MTLLoader } from "three/examples/jsm/Addons.js";
 
-export default class DemoScene extends THREE.Scene
-{
-    private readonly objLoader = new OBJLoader()
-    private readonly mtlLoader = new MTLLoader()
+export default class DemoScene extends THREE.Scene {
+    private readonly objLoader = new OBJLoader();
+    private readonly mtlLoader = new MTLLoader();
+    private bunny = new THREE.Object3D();
 
-    async initialize()
-    {
-        const targetMtl = await this.mtlLoader.loadAsync('assets/targetA.mtl')
-        targetMtl.preload()
+    constructor() {
+        super();
+    }
 
-        const bunny = await this.createBunny(targetMtl)
-        bunny.scale.set(10, 10, 10)
-        this.add(bunny)
+    async initialize(callback: Function) {
+        const targetMtl = await this.mtlLoader.loadAsync("assets/targetA.mtl");
+        targetMtl.preload();
 
-        const light = new THREE.DirectionalLight(0xFFFFFF, 1)
-        light.position.set(0, 4, 2)
-        this.add(light)
+        this.bunny = await this.createBunny(targetMtl);
+        this.bunny.scale.set(10, 10, 10);
+        this.add(this.bunny);
+
+        const bbox = new THREE.Box3().setFromObject(this.bunny);
+        const helper = new THREE.Box3Helper(bbox, 0xffff00);
+        this.add(helper);
+
+        const light = new THREE.DirectionalLight(0xffffff, 1);
+        light.position.set(0, 4, 2);
+        this.add(light);
+
+        callback();
     }
 
     private async createBunny(mtl: MTLLoader.MaterialCreator) {
-        this.objLoader.setMaterials(mtl)
+        this.objLoader.setMaterials(mtl);
 
-        const modelRoot = await this.objLoader.loadAsync('assets/stanford-bunny.obj')
+        const modelRoot = await this.objLoader.loadAsync(
+            "assets/stanford-bunny.obj"
+        );
 
-        return modelRoot
+        return modelRoot;
     }
 
-    update() {
+    update() {}
+
+    getVertices(): THREE.Float32BufferAttribute[] {
+        const buffers: THREE.Float32BufferAttribute[] = [];
+        this.traverse(function (o) {
+            if (o instanceof THREE.Mesh) {
+                buffers.push(o.geometry.getAttribute("position"));
+            }
+        });
+
+        return buffers;
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,35 @@
 // import './style.css'
-import * as THREE from 'three';
-import DemoScene from './DemoScene';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import DemoScene from "./DemoScene";
 
 const scene = new DemoScene();
-scene.initialize()
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+scene.initialize(sceneLoaded);
+
+const camera = new THREE.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 camera.position.z = 5;
+controls.update();
 
-function animate() {
-
-  scene.update()
-  requestAnimationFrame( animate );
-	renderer.render( scene, camera );
+function sceneLoaded() {
+    console.log(scene.getVertices());
+    animate();
 }
 
-animate();
+function animate() {
+    controls.update();
+    scene.update();
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
