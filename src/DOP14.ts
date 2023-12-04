1ae4f4c2278c7b9f4be3fff221757dbccf16912f
Pungitius <robert.kosmehl@gmx.de>
2023-12-04 13:10:03 +0100

    add: bad applyMatrix4 method implementation

--- src/DOP14.ts
+++ src/DOP14.ts
@@ -25,8 +25,224 @@ export default class DOP14 {
         this.normals.forEach((n) => n.normalize());
     }
 
-    applyMatrix(_matrix: THREE.Matrix4): this {
-        throw new Error("not Implemented");
+    applyMatrix4(matrix: THREE.Matrix4): this {
+        function getIntersectionLine(
+            plane1Normal: THREE.Vector3,
+            plane1Distance: number,
+            plane2Normal: THREE.Vector3,
+            plane2Distance: number
+        ) {
+            const direction = plane1Normal
+                .clone()
+                .cross(plane2Normal)
+                .normalize();
+
+            // https://matematicainteractivacr.com/practicasapuntes/ArtticuloFormulaInterseccionplanos.pdf
+            // https://stackoverflow.com/questions/6408670/line-of-intersection-between-two-planes#:~:text=0-,You,-can%20find%20the
+            const n1 = plane1Normal;
+            const n2 = plane2Normal;
+            const d1 = plane1Normal
+                .clone()
+                .dot(plane1Normal.clone().multiplyScalar(plane1Distance));
+            const d2 = plane2Normal
+                .clone()
+                .dot(plane2Normal.clone().multiplyScalar(plane2Distance));
+
+            const n12 = n1.clone().cross(n2).length() ** 2;
+
+            if (n12 == 0) {
+                console.log("parallel vectors");
+            }
+
+            const a1 = (d1 * n2.length() ** 2 - d2 * n1.clone().dot(n2)) / n12;
+            const a2 = (d2 * n1.length() ** 2 - d1 * n1.clone().dot(n2)) / n12;
+            const p = n1
+                .clone()
+                .multiplyScalar(a1)
+                .add(n2.clone().multiplyScalar(a2));
+
+            // console.log(p);
+
+            return {
+                direction: direction,
+                start: p,
+            };
+        }
+
+        function getLinePlaneIntersectionU(
+            planeNormal: THREE.Vector3,
+            planeDistance: number,
+            lineStart: THREE.Vector3,
+            lineDirection: THREE.Vector3
+        ): number {
+            if (
+                Math.abs(
+                    planeNormal.clone().angleTo(lineDirection) - Math.PI / 2
+                ) < 0.001
+            ) {
+                // console.log('line is parallel to plane')
+                return Infinity;
+            }
+
+            const planePoint = planeNormal
+                .clone()
+                .multiplyScalar(planeDistance);
+            const u =
+                (planeNormal.clone().dot(planePoint.clone()) -
+                    planeNormal.clone().dot(lineStart.clone())) /
+                planeNormal.clone().dot(lineDirection.clone().normalize());
+
+            return u;
+        }
+
+        function pointIsOnLeftPlaneSide(
+            normal: THREE.Vector3,
+            distanceToOrigin: number,
+            point: THREE.Vector3
+        ): boolean {
+            const pointOnPlane = normal
+                .clone()
+                .multiplyScalar(distanceToOrigin);
+            const d = -normal.clone().dot(pointOnPlane);
+            return point.clone().dot(normal) + d > 0;
+        }
+
+        const lineSegments = [];
+        // for all combinations of planes
+        for (let i = 0; i < this.normals.length; i++) {
+            for (let j = 0; j < this.normals.length; j++) {
+                if (i == j) continue;
+
+                const l1 = getIntersectionLine(
+                    this.normals[i],
+                    this.min[i],
+                    this.normals[j],
+                    this.min[j]
+                );
+
+                const l2 = getIntersectionLine(
+                    this.normals[i],
+                    this.min[i],
+                    this.normals[j],
+                    this.max[j]
+                );
+
+                const l3 = getIntersectionLine(
+                    this.normals[i],
+                    this.max[i],
+                    this.normals[j],
+                    this.min[j]
+                );
+
+                const l4 = getIntersectionLine(
+                    this.normals[i],
+                    this.max[i],
+                    this.normals[j],
+                    this.max[j]
+                );
+
+                for (const l of [l1, l2, l3, l4]) {
+                    // let minU = Number.MIN_VALUE;
+                    // let maxU = Number.MAX_VALUE;
+
+                    const bounds = 10000; // temporary
+                    let minU = -bounds;
+                    let maxU = bounds;
+                    for (let k = 0; k < this.normals.length; k++) {
+                        if (k == j || k == i) continue;
+
+                        const u1 = getLinePlaneIntersectionU(
+                            this.normals[k],
+                            this.min[k],
+                            l.start,
+                            l.direction
+                        );
+
+                        const u2 = getLinePlaneIntersectionU(
+                            this.normals[k],
+                            this.max[k],
+                            l.start,
+                            l.direction
+                        );
+
+                        // if the line is parallel to a slab it has to be inside of the slab
+                        if (!isFinite(u1)) {
+                            if (
+                                !pointIsOnLeftPlaneSide(
+                                    this.normals[k],
+                                    this.min[k],
+                                    l.start
+                                )
+                            ) {
+                                minU = 1; // hacky way to exclude this line
+                                maxU = 0;
+                                break;
+                            }
+                        }
+
+                        if (!isFinite(u2)) {
+                            if (
+                                pointIsOnLeftPlaneSide(
+                                    this.normals[k],
+                                    this.max[k],
+                                    l.start
+                                )
+                            ) {
+                                minU = 1; // hacky way to exclude this line
+                                maxU = 0;
+                                break;
+                            }
+                        }
+
+                        if (u1 > u2) {
+                            if (isFinite(u1)) {
+                                minU = Math.max(minU, u2);
+                            }
+
+                            if (isFinite(u2)) {
+                                maxU = Math.min(maxU, u1);
+                            }
+                        } else {
+                            if (isFinite(u2)) {
+                                minU = Math.max(minU, u1);
+                            }
+
+                            if (isFinite(u1)) {
+                                maxU = Math.min(maxU, u2);
+                            }
+                        }
+                    }
+
+                    if (minU > maxU) {
+                        // line segment is overshortened
+                        continue;
+                    }
+
+                    const p1 = l.start
+                        .clone()
+                        .add(l.direction.clone().multiplyScalar(minU));
+                    const p2 = l.start
+                        .clone()
+                        .add(l.direction.clone().multiplyScalar(maxU));
+
+                    lineSegments.push([p1, p2]);
+                }
+            }
+        }
+
+        const representativePoints: THREE.Vector3[] = []
+        for (const ls of lineSegments) {
+            representativePoints.push(ls[0], ls[1])
+        }
+
+        const transformedRepresentativePoints: THREE.Vector3[] = []
+        for (const rp of representativePoints) {
+            transformedRepresentativePoints.push(rp.applyMatrix4(matrix))
+        }
+        
+        this.setFromPoints( transformedRepresentativePoints );
+
+        return this;
     }
 
     clone(): DOP14 {
@@ -88,7 +304,7 @@ export default class DOP14 {
 
         object.updateWorldMatrix(false, false);
 
-        const geometry = (object as THREE.Mesh).geometry;
+            const geometry = (object as THREE.Mesh).geometry;
 
         if (geometry !== undefined) {
             const positionAttribute = geometry.getAttribute("position");
@@ -151,7 +367,7 @@ export default class DOP14 {
         throw new Error("not Implemented");
     }
 
-    setFromPoints(_points: THREE.Vector3): this {
+    setFromPoints(_points: THREE.Vector3[]): this {
         throw new Error("not Implemented");
     }
 
