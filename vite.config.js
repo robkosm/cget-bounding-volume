290dda534c3eab310e4a460677fda8882323d572
Pungitius <robert.kosmehl@gmx.de>
2024-02-02 16:19:53 +0100

    test: add vite pages

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+import VitePages from 'vite-plugin-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [VitePages()],
   base: '/cget-bounding-volume/'
 })
