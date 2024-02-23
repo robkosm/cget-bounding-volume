f42133fa9a899fe1473b5c6f304777b18b559066
Pungitius <robert.kosmehl@gmx.de>
2024-02-23 19:56:21 +0100

    main menu message

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,10 @@
 function Menu() {
-    return <p> meu </p>;
+    return (
+        <h1 className="font-sans text-4xl">
+            {" "}
+            an empty vessel makes the most noise{" "}
+        </h1>
+    );
 }
 
 export default Menu;
